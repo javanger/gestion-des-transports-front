50269705f6b3508e6fac4d00f57af0bb1b8e09ad
Valentin <lefebvre.valentin76@gmail.com>
2018-04-27 14:48:17 +0200

    Autocomplétion des adresses via l'API Google Map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CreerAnnonceComponent } from './pages/creer-annonce/creer-annonce.component';
@@ -17,7 +18,13 @@ import { ItineraireCreationComponent } from './shared/itineraire-creation/itiner
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    FormsModule,
+    ReactiveFormsModule,
+    MDBBootstrapModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyAYzXigARjswquPe9Jz4bz1M50GKf2DMoI",
+      libraries: ["places"]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/itineraire-creation/itineraire-creation.component.ts
+++ src/app/shared/itineraire-creation/itineraire-creation.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, NgZone, OnInit, ViewChild, Component } from '@angular/core';
+import { } from 'googlemaps';
+import { FormControl } from '@angular/forms';
+import { MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'itineraire-creation',
@@ -9,10 +13,33 @@ export class ItineraireCreationComponent implements OnInit {
 
   duree:string = "Ici la durée prochainement"
   distance:string ="ici bientot la distance"
+  
+  public searchControl: FormControl;
 
-  constructor() { }
+  @ViewChild("searchDepart")
+  public searchElementRefDep: ElementRef;
+
+  @ViewChild("searchArrive")
+  public searchElementRefArr: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader
+  ) {}
 
   ngOnInit() {
+    //set google maps defaults
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocompleteDep = new google.maps.places.Autocomplete(this.searchElementRefDep.nativeElement, {
+        types: ["address"]
+      });
+      let autocompleteArr = new google.maps.places.Autocomplete(this.searchElementRefArr.nativeElement, {
+        types: ["address"]
+      });
+    });
   }
 
 }
