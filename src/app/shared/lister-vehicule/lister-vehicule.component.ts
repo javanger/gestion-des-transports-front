3619e6e0c4cd0d6e6b262204d96700098e483feb
victorahs <victorahsene90@gmail.com>
2018-04-30 14:12:19 +0200

    suppression debug

--- src/app/shared/lister-vehicule/lister-vehicule.component.ts
+++ src/app/shared/lister-vehicule/lister-vehicule.component.ts
@@ -1,27 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Vehicule, CategorieVehicule } from '../../model';
-import { VehiculeService } from '../../services/vehicule.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Vehicule, CategorieVehicule } from "../../model";
+import { VehiculeService } from "../../services/vehicule.service";
 
 @Component({
-  selector: 'app-lister-vehicule',
-  templateUrl: './lister-vehicule.component.html',
-  styleUrls: ['./lister-vehicule.component.scss']
+  selector: "app-lister-vehicule",
+  templateUrl: "./lister-vehicule.component.html",
+  styleUrls: ["./lister-vehicule.component.scss"]
 })
 export class ListerVehiculeComponent implements OnInit {
-  @Input() vehicules : Array<Vehicule>;
-  constructor(private sVehicule:VehiculeService) { }
+  @Input() vehicules: Array<Vehicule>;
+  constructor(private sVehicule: VehiculeService) {}
 
   ngOnInit() {
-    this.sVehicule.listerVehicules().subscribe((data:any) => {
+    this.sVehicule.listerVehicules().subscribe((data: any) => {
       this.vehicules = data;
-
-      this.vehicules.forEach(e => {
-
-
-        
-      });
-      
-  })
-
-}
+    });
+  }
 }
