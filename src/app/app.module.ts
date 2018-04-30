30584d3f448c074a0d2977d674a043c0bc99b419
Axel Buteau <buteauaxel@gmail.com>
2018-04-30 10:02:24 +0200

    merge 03menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './shared/connexion/connexion.component';
+import { DeconnexionComponent } from './shared/deconnexion/deconnexion.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    DeconnexionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
