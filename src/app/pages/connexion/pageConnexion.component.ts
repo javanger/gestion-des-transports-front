7ab5596b16e687753d7a9d155717f40965681013
Guillaume <guillaume.gobert@gmail.com>
2018-05-04 15:56:46 +0200

    correction de la page connexion

---
+++ src/app/pages/connexion/pageConnexion.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'page-connexion',
+  templateUrl: './pageConnexion.component.html',
+  styleUrls: ['./pageConnexion.component.scss']
+})
+export class PageConnexionComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
